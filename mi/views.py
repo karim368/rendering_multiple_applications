9313e8d197ae1fee57e5209448bce30ccd2ecef4
karim368 <karimever251@gmail.com>
2024-01-19 21:59:30 +0530

    first commit

---
+++ mi/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def rohith(request):
+    return render(request,'rohith.html')
+
+def surya(request):
+    return render(request,'surya.html')
